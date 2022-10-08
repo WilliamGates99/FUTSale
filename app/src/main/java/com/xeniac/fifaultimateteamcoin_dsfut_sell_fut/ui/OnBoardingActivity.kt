54b7691f44db9f2953bb3efd9e052e8b583bdb3c
William Gates <william.gates.3299@gmail.com>
2022-10-08 23:44:15 +0330

    Improved OnBoarding Screens UI

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/ui/OnBoardingActivity.kt
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.databinding.ActivityOnboardingBinding
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.ui.adapters.DotsIndicatorAdapter
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.ui.fragments.onboarding.OnBoardingFirstFragment
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.ui.fragments.onboarding.OnBoardingFourthFragment
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.ui.fragments.onboarding.OnBoardingSecondFragment
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.ui.fragments.onboarding.OnBoardingThirdFragment
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.ui.viewmodels.OnBoardingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -42,13 +47,33 @@ class OnBoardingActivity : AppCompatActivity() {
             }
         }
 
-    private fun onBoardingInit() {
-        binding = ActivityOnboardingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
     private fun navigateToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+    }
+
+    private fun onBoardingInit() {
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewPager()
+    }
+
+    // TODO BACK PRESSED CALLBACK TO SWIPE TO PREVIOUS VIEWPAGER PAGE
+
+    private fun setupViewPager() {
+        val fragmentsList = arrayListOf(
+            OnBoardingFirstFragment(),
+            OnBoardingSecondFragment(),
+            OnBoardingThirdFragment(),
+            OnBoardingFourthFragment()
+        )
+
+        val dotsIndicatorAdapter = DotsIndicatorAdapter(
+            fragmentsList, supportFragmentManager, lifecycle
+        )
+
+        binding.viewpager.adapter = dotsIndicatorAdapter
+        binding.indicator.attachTo(binding.viewpager)
     }
 }
