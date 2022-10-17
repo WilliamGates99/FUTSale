2c30461b799164b5b790e697b59cf447c7ae782e
William Gates <william.gates.3299@gmail.com>
2022-10-17 18:56:44 +0330

    Fixed Issue #2

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/utils/LinkHelper.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/utils/LinkHelper.kt
@@ -1,5 +1,6 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.utils
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -8,22 +9,24 @@ import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.utils.SnackbarHelper.showI
 
 object LinkHelper {
 
-    fun openLink(context: Context, view: View, urlString: String) = Intent().apply {
-        action = Intent.ACTION_VIEW
-        data = Uri.parse(urlString)
-
-        resolveActivity(context.packageManager)?.let {
+    fun openLink(context: Context, view: View, urlString: String) = try {
+        Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(urlString)
             context.startActivity(this)
-        } ?: showIntentAppNotFoundError(context, view)
+        }
+    } catch (e: ActivityNotFoundException) {
+        showIntentAppNotFoundError(context, view)
     }
 
-//    fun openPlayStore(context: Context, view: View) = Intent().apply {
-//        action = Intent.ACTION_VIEW
-//        data = Uri.parse(BuildConfig.URL_APP_STORE)
-//        setPackage(BuildConfig.PACKAGE_NAME_APP_STORE)
-//
-//        resolveActivity(context.packageManager)?.let {
+//    fun openPlayStore(context: Context, view: View) = try {
+//        Intent().apply {
+//            action = Intent.ACTION_VIEW
+//            data = Uri.parse(BuildConfig.URL_APP_STORE)
+//            setPackage(BuildConfig.PACKAGE_NAME_APP_STORE)
 //            context.startActivity(this)
-//        } ?: openLink(context, view, BuildConfig.URL_APP_STORE)
+//        }
+//    } catch (e: ActivityNotFoundException) {
+//        openLink(context, view, BuildConfig.URL_APP_STORE)
 //    }
 }
