c07d524e53cbeeedc81c3237020a5af1c37383f5
William Gates <william.gates.3299@gmail.com>
2023-01-12 18:14:13 +0330

    v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.xeniac.fifaultimateteamcoin_dsfut_sell_fut"
         minSdk = 21
         targetSdk = 33
-        versionCode = 5 // TODO UPGRADE FOR RELEASE
-        versionName = "1.0.4" // TODO UPGRADE FOR RELEASE
+        versionCode = 6 // TODO UPGRADE AFTER EACH RELEASE
+        versionName = "1.1.0" // TODO UPGRADE AFTER EACH RELEASE
 
         /**
          * Keeps language resources for only the locales specified below.
