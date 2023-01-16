ccc0040768e0e141160c08d3900b4c97504201b6
William Gates <william.gates.3299@gmail.com>
2023-01-16 17:31:03 +0330

    Updated Kotlin to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@
 plugins {
     id("com.android.application") version "7.4.0" apply false // Android Gradle Plugin Version
     id("com.android.library") version "7.4.0" apply false // Android Gradle Plugin Version
-    id("org.jetbrains.kotlin.android") version "1.7.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("androidx.navigation.safeargs") version "2.5.3" apply false
     id("com.google.gms.google-services") version "4.3.14" apply false // Google Services plugin
     id("com.google.dagger.hilt.android") version "2.44.2" apply false
