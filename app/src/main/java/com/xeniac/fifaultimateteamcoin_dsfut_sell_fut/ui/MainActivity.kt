117974f88e54cb559cdd1b55abe26a447252c7b4
William Gates <william.gates.3299@gmail.com>
2023-02-01 20:19:02 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/ui/MainActivity.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/ui/MainActivity.kt
@@ -66,6 +66,7 @@ class MainActivity : AppCompatActivity(), MaxAdListener {
             mainInit()
             shouldShowSplashScreen = false
         } else {
+            shouldShowSplashScreen = false
             navigateToOnBoardingActivity()
         }
     }
