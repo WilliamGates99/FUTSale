e364fc4d4e7fc23158a7a2281cd08b5dec5d086e
William Gates <william.gates.3299@gmail.com>
2023-02-02 20:33:56 +0330

    Wrote MainViewModel Tests

--- app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/ui/viewmodels/MainViewModelTest.kt
+++ app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/ui/viewmodels/MainViewModelTest.kt
@@ -5,6 +5,7 @@ import com.google.common.truth.Truth.assertThat
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.MainCoroutineRule
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.data.repository.FakePreferencesRepositoryImp
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.domain.repository.PreferencesRepository
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
@@ -29,10 +30,38 @@ class MainViewModelTest {
     }
 
     @Test
-    fun getIsOnBoardingCompleted_returnsDefaultIsOnBoardingCompleted() {
+    fun isOnBoardingCompleted_returnsDefaultIsOnBoardingCompletedValue() {
         val defaultIsOnBoardingCompleted = false
 
         val isOnBoardingCompleted = testViewModel.isOnBoardingCompleted()
         assertThat(isOnBoardingCompleted).isEqualTo(defaultIsOnBoardingCompleted)
+    }
+
+    @Test
+    fun getRateAppDialogChoice_returnsDefaultRateAppDialogChoice() {
+        val defaultRateAppDialogChoice = 0
+        testViewModel.getRateAppDialogChoice()
+
+        val responseEvent = testViewModel.rateAppDialogChoiceLiveData.getOrAwaitValue()
+        assertThat(responseEvent.getContentIfNotHandled()).isEqualTo(defaultRateAppDialogChoice)
+    }
+
+    @Test
+    fun getPreviousRequestTimeInMillis_returnsDefaultPreviousRequestTimeInMillis() {
+        val defaultPreviousRequestTimeInMillis = 0L
+        testViewModel.getPreviousRequestTimeInMillis()
+
+        val responseEvent = testViewModel.previousRequestTimeInMillisLiveData.getOrAwaitValue()
+        assertThat(responseEvent.getContentIfNotHandled())
+            .isEqualTo(defaultPreviousRequestTimeInMillis)
+    }
+
+    @Test
+    fun setRateAppDialogChoice_returnsNewRateAppDialogChoice() {
+        val newRateAppDialogChoice = 1
+        testViewModel.setRateAppDialogChoice(newRateAppDialogChoice)
+
+        val responseEvent = testViewModel.rateAppDialogChoiceLiveData.getOrAwaitValue()
+        assertThat(responseEvent.getContentIfNotHandled()).isEqualTo(newRateAppDialogChoice)
     }
 }
