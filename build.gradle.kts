0f6dda4c0887f52c767658298ea1f478ec0380a7
William Gates <william.gates.3299@gmail.com>
2023-02-03 15:17:44 +0330

    Updated Android Gradle Plugin Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@
 //}
 
 plugins {
-    id("com.android.application") version "7.4.0" apply false // Android Gradle Plugin Version
-    id("com.android.library") version "7.4.0" apply false // Android Gradle Plugin Version
+    id("com.android.application") version "7.4.1" apply false // Android Gradle Plugin Version
+    id("com.android.library") version "7.4.1" apply false // Android Gradle Plugin Version
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("androidx.navigation.safeargs") version "2.5.3" apply false
     id("com.google.gms.google-services") version "4.3.14" apply false // Google Services plugin
