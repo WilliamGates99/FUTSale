876a4b0b1678a860d6d24b3fa8d26f0726e39917
William Gates <william.gates.3299@gmail.com>
2024-01-18 17:20:41 +0330

    Update Gradle Files for Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,21 +7,19 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.devtools.ksp")
-    id("kotlin-kapt")
     id("kotlin-parcelize")
     id("dagger.hilt.android.plugin")
-    id("androidx.navigation.safeargs.kotlin")
     id("com.google.gms.google-services") // Google Services plugin
     id("com.google.firebase.crashlytics")
     id("com.google.firebase.firebase-perf")
-    id("applovin-quality-service")
+    // id("applovin-quality-service")
 }
 
 val properties = gradleLocalProperties(rootDir)
 
-applovin {
-    apiKey = properties.getProperty("APPLOVIN_API_KEY")
-}
+// applovin {
+//     apiKey = properties.getProperty("APPLOVIN_API_KEY")
+// }
 
 android {
     namespace = "com.xeniac.fifaultimateteamcoin_dsfut_sell_fut"
@@ -40,79 +38,88 @@ android {
 
         testInstrumentationRunner = "com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.HiltTestRunner"
 
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
         buildConfigField(
             "String",
-            "RETROFIT_BASE_URL",
-            properties.getProperty("RETROFIT_BASE_URL")
+            "KTOR_HTTP_BASE_URL",
+            properties.getProperty("KTOR_HTTP_BASE_URL")
         )
-        buildConfigField(
-            "String",
-            "RETROFIT_FEED_URL",
-            properties.getProperty("RETROFIT_FEED_URL")
-        )
+
+        /*
         buildConfigField(
             "String",
             "APPLOVIN_INTERSTITIAL_UNIT_ID",
             properties.getProperty("APPLOVIN_INTERSTITIAL_UNIT_ID")
         )
+
         buildConfigField(
             "String",
             "APPLOVIN_PROFILE_NATIVE_UNIT_ID",
             properties.getProperty("APPLOVIN_PROFILE_NATIVE_UNIT_ID")
         )
+
         buildConfigField(
             "String",
             "APPLOVIN_SETTINGS_NATIVE_UNIT_ID",
             properties.getProperty("APPLOVIN_SETTINGS_NATIVE_UNIT_ID")
         )
+
         buildConfigField(
             "String",
             "APPLOVIN_MISCELLANEOUS_NATIVE_ZONE_ID",
             properties.getProperty("APPLOVIN_MISCELLANEOUS_NATIVE_ZONE_ID")
         )
+
         buildConfigField(
             "String",
             "TAPSELL_KEY",
             properties.getProperty("TAPSELL_KEY")
         )
+
         buildConfigField(
             "String",
             "TAPSELL_INTERSTITIAL_ZONE_ID",
             properties.getProperty("TAPSELL_INTERSTITIAL_ZONE_ID")
         )
+
         buildConfigField(
             "String",
             "TAPSELL_PROFILE_NATIVE_ZONE_ID",
             properties.getProperty("TAPSELL_PROFILE_NATIVE_ZONE_ID")
         )
+
         buildConfigField(
             "String",
             "TAPSELL_SETTINGS_NATIVE_ZONE_ID",
             properties.getProperty("TAPSELL_SETTINGS_NATIVE_ZONE_ID")
         )
+
         buildConfigField(
             "String",
             "TAPSELL_MISCELLANEOUS_NATIVE_ZONE_ID",
             properties.getProperty("TAPSELL_MISCELLANEOUS_NATIVE_ZONE_ID")
         )
+        */
     }
 
     buildTypes {
-        getByName("debug") {
+        debug {
             versionNameSuffix = " - debug"
             applicationIdSuffix = ".debug"
 
-            resValue("color", "appIconBackground", "#FF9100") // Orange
+            resValue(
+                type = "color",
+                name = "appIconBackground",
+                value = "#FFFF9100" // Orange
+            )
         }
 
-        getByName("release") {
-            // Enables code shrinking, obfuscation, and optimization for only your project's release build type.
+        release {
             isMinifyEnabled = true
-
-            // Enables resource shrinking, which is performed by the Android Gradle plugin.
             isShrinkResources = true
-
-            // Includes the default ProGuard rules files that are packaged with the Android Gradle plugin.
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -126,21 +133,33 @@ android {
             dimension = "build"
             versionNameSuffix = " - Developer Preview"
             applicationIdSuffix = ".dev"
-            resValue("color", "appIconBackground", "#FF0011") // Red
+
+            resValue(
+                type = "color",
+                name = "appIconBackground",
+                value = "#FFFF0011" // Red
+            )
         }
 
         create("prod") {
             dimension = "build"
-            resValue("color", "appIconBackground", "#10161A") // Dark Gray
+
+            resValue(
+                type = "color",
+                name = "appIconBackground",
+                value = "#FF0C160D" // Dark Green
+            )
         }
 
         create("playStore") {
             dimension = "market"
+
             buildConfigField(
                 "String",
                 "URL_APP_STORE",
                 "\"https://play.google.com/store/apps/details?id=com.xeniac.fifaultimateteamcoin_dsfut_sell_fut\""
             )
+
             buildConfigField(
                 "String",
                 "PACKAGE_NAME_APP_STORE",
@@ -150,11 +169,13 @@ android {
 
         create("cafeBazaar") {
             dimension = "market"
+
             buildConfigField(
                 "String",
                 "URL_APP_STORE",
                 "\"https://cafebazaar.ir/app/com.xeniac.fifaultimateteamcoin_dsfut_sell_fut\""
             )
+
             buildConfigField(
                 "String",
                 "PACKAGE_NAME_APP_STORE",
@@ -164,11 +185,13 @@ android {
 
         create("myket") {
             dimension = "market"
+
             buildConfigField(
                 "String",
                 "URL_APP_STORE",
                 "\"https://myket.ir/app/com.xeniac.fifaultimateteamcoin_dsfut_sell_fut\""
             )
+
             buildConfigField(
                 "String",
                 "PACKAGE_NAME_APP_STORE",
@@ -179,8 +202,7 @@ android {
 
     buildFeatures {
         buildConfig = true
-        viewBinding = true
-        dataBinding = true
+        compose = true
     }
 
     compileOptions {
@@ -193,6 +215,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     packaging {
@@ -240,14 +266,13 @@ androidComponents {
     }
 }
 
-kapt {
-    // Allow references to generated code
-    correctErrorTypes = true
-}
-
 ksp {
     // Export room db schemas
-    arg(RoomSchemaArgProvider(schemaDir = File(projectDir, "roomDbSchemas")))
+    arg(RoomSchemaArgProvider(File(projectDir, "roomSchemas")))
+}
+
+hilt {
+    enableAggregatingTask = true
 }
 
 class RoomSchemaArgProvider(
@@ -262,71 +287,89 @@ class RoomSchemaArgProvider(
 
 dependencies {
     // Java 8+ API Desugaring Support
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
-    // Navigation Component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+    // Jetpack Compose
+    val composeBoM = platform("androidx.compose:compose-bom:2023.10.01")
+    implementation(composeBoM)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3") // Material Design 3
+    implementation("androidx.compose.runtime:runtime-livedata") // Compose Integration with LiveData
+    implementation("androidx.activity:activity-compose:1.8.2") // Compose Integration with Activities
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0") // Compose Navigation Integration with Hilt
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1") // Compose Constraint Layout
+
+    // Android Studio Compose Preview Support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.6")
 
     // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
-
-    // Activity KTX for Injecting ViewModels into Fragments
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("com.google.dagger:hilt-android:2.50")
+    ksp("com.google.dagger:hilt-compiler:2.50")
 
     // Architectural Components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0") // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0") // ViewModel Utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0") // Lifecycles Only (without ViewModel or LiveData)
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0") // Lifecycle Utilities for Compose
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Room Library
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2") // Kotlin Extensions and Coroutines support for Room
-    ksp("androidx.room:room-compiler:2.5.2")
+    // Coroutines Support for Firebase
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
-    // Kotlin Extensions and Coroutines Support for Room
-    implementation("androidx.room:room-ktx:2.5.2")
+    // Ktor Client Library
+    implementation("io.ktor:ktor-client-core:2.3.7")
+    implementation("io.ktor:ktor-client-okhttp:2.3.7") // Ktor OkHttp Engine
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("io.ktor:ktor-client-logging:2.3.7")
+
+    // Kotlin JSON Serialization Library
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+
+    // Room Library
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1") // Kotlin Extensions and Coroutines support for Room
+    ksp("androidx.room:room-compiler:2.6.1")
 
     // Preferences DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     // Firebase BoM and Analytics
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+
+    // Firebase Cloud Messaging
+    implementation("com.google.firebase:firebase-messaging-ktx")
 
     // Firebase Release & Monitor
     implementation("com.google.firebase:firebase-crashlytics-ktx")
     implementation("com.google.firebase:firebase-perf-ktx")
 
-    // Retrofit Library
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
-
     // Timber Library
     implementation("com.jakewharton.timber:timber:5.0.1")
 
-    // EasyPermissions Library
-    implementation("com.vmadalin:easypermissions-ktx:1.0.0")
-
     // Lottie Library
-    implementation("com.airbnb.android:lottie:6.1.0")
+    implementation("com.airbnb.android:lottie-compose:6.3.0")
 
-    // Dots Indicator Library
-    implementation("com.tbuonomo:dotsindicator:5.0")
+    // Coil Library
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("io.coil-kt:coil-svg:2.5.0")
+    implementation("io.coil-kt:coil-gif:2.5.0")
 
     // Google Play In-App Reviews API
     implementation("com.google.android.play:review-ktx:2.0.1")
 
+    /*
     // Applovin Libraries
     implementation("com.applovin:applovin-sdk:11.11.3")
     implementation("com.google.android.gms:play-services-ads-identifier:18.0.1")
@@ -337,37 +380,35 @@ dependencies {
 
     // Tapsell Library
     implementation("ir.tapsell.plus:tapsell-plus-sdk-android:2.2.0")
+    */
 
     // Local Unit Test Libraries
-    testImplementation("com.google.truth:truth:1.1.5")
+    testImplementation("com.google.truth:truth:1.2.0")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0") // Test Helpers for LiveData
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("androidx.room:room-testing:2.6.1")
 
     // Instrumentation Test Libraries
-    androidTestImplementation("com.google.truth:truth:1.1.5")
+    androidTestImplementation("com.google.truth:truth:1.2.0")
     androidTestImplementation("junit:junit:4.13.2")
-    //noinspection GradleDependency
-    androidTestImplementation("androidx.test:core:1.4.0") // DO NOT UPGRADE
+    androidTestImplementation("androidx.test:core:1.5.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0") // Test Helpers for LiveData
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
-    kaptAndroidTest("com.google.dagger:hilt-compiler:2.48")
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.50")
+    kspAndroidTest("com.google.dagger:hilt-compiler:2.50")
 
     // UI Test Libraries
-    androidTestImplementation("androidx.navigation:navigation-testing:2.7.2")
-    //noinspection GradleDependency
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0") // DO NOT UPGRADE
-    //noinspection GradleDependency
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.3.0") // DO NOT UPGRADE
-    //noinspection GradleDependency
-    androidTestImplementation("androidx.test.espresso:espresso-intents:3.3.0") // DO NOT UPGRADE
-    debugImplementation("androidx.fragment:fragment-testing:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(composeBoM)
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
 val releaseRootDir = "${rootDir}/app"
-val destDir: String = properties.getProperty("DESTINATION_DIR")
+val apkDestDir: String = properties.getProperty("APK_DESTINATION_DIR")
+val bundleDestDir: String = properties.getProperty("BUNDLE_DESTINATION_DIR")
 val obfuscationDestDir: String = properties.getProperty("OBFUSCATION_DESTINATION_DIR")
 
 val versionName = "${android.defaultConfig.versionName}"
@@ -378,7 +419,7 @@ tasks.register<Copy>("copyDevPreviewBundle") {
     val bundleSourceDir = "${releaseRootDir}/devPlayStore/release/${bundleFile}"
 
     from(bundleSourceDir)
-    into(destDir)
+    into(bundleDestDir)
 
     rename(bundleFile, "$renamedFileName (Developer Preview).aab")
 }
@@ -388,7 +429,7 @@ tasks.register<Copy>("copyDevPreviewApk") {
     val apkSourceDir = "${releaseRootDir}/devPlayStore/release/${apkFile}"
 
     from(apkSourceDir)
-    into(destDir)
+    into(apkDestDir)
 
     rename(apkFile, "$renamedFileName (Developer Preview).aab")
 }
@@ -401,10 +442,10 @@ tasks.register<Copy>("copyReleaseApk") {
     val myketApkSourceDir = "${releaseRootDir}/prodMyket/release/${myketApkFile}"
 
     from(cafeBazaarApkSourceDir)
-    into(destDir)
+    into(apkDestDir)
 
     from(myketApkSourceDir)
-    into(destDir)
+    into(apkDestDir)
 
     rename(cafeBazaarApkFile, "$renamedFileName - CafeBazaar.apk")
     rename(myketApkFile, "$renamedFileName - Myket.apk")
@@ -415,7 +456,7 @@ tasks.register<Copy>("copyReleaseBundle") {
     val playStoreBundleSourceDir = "${releaseRootDir}/prodPlayStore/release/${playStoreBundleFile}"
 
     from(playStoreBundleSourceDir)
-    into(destDir)
+    into(bundleDestDir)
 
     rename(playStoreBundleFile, "${renamedFileName}.aab")
 }
