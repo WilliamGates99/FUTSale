4ef24203252fc9fbaf8f01eb66eca7f46dd47052
William Gates <william.gates.3299@gmail.com>
2024-01-19 20:17:30 +0330

    Implement Onboarding Screens

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/presentation/OnboardingScreen.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/presentation/OnboardingScreen.kt
@@ -1,28 +1,75 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_onboarding.presentation
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBarsIgnoringVisibility
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.R
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun OnboardingScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = stringResource(id = R.string.onboarding_first_description),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 24.dp)
+fun OnboardingScreen(
+    onNavigateToHomeScreen: () -> Unit
+) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val horizontalPadding by remember { derivedStateOf { 36.dp } }
+
+    LaunchedEffect(key1 = Unit) {
+        val snackbarResult = snackbarHostState.showSnackbar(
+            message = context.getString(R.string.onboarding_first_description),
+            actionLabel = context.getString(R.string.error_btn_retry),
+            withDismissAction = true,
+            duration = SnackbarDuration.Indefinite
         )
+
+        when (snackbarResult) {
+            SnackbarResult.ActionPerformed -> {
+
+            }
+            SnackbarResult.Dismissed -> {
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.windowInsetsPadding(WindowInsets.navigationBarsIgnoringVisibility)
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.ime)
+                .padding(
+                    top = innerPadding.calculateTopPadding(),
+                    bottom = innerPadding.calculateBottomPadding()
+                )
+        ) {
+
+        }
     }
 }
