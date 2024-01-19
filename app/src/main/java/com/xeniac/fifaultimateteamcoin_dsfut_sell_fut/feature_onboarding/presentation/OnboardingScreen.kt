bf46913b009cd8d7c1360b09aa34797b4ac7a647
William Gates <william.gates.3299@gmail.com>
2024-01-19 21:12:22 +0330

    Implement Onboarding Screens

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/presentation/OnboardingScreen.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/presentation/OnboardingScreen.kt
@@ -1,20 +1,18 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_onboarding.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.navigationBarsIgnoringVisibility
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -22,9 +20,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.R
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.ui.theme.surfaceContainerDark
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.ui.theme.surfaceContainerLight
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun OnboardingScreen(
     onNavigateToHomeScreen: () -> Unit
@@ -32,33 +30,14 @@ fun OnboardingScreen(
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
+    val isSystemInDarkTheme = isSystemInDarkTheme()
     val horizontalPadding by remember { derivedStateOf { 36.dp } }
 
-    LaunchedEffect(key1 = Unit) {
-        val snackbarResult = snackbarHostState.showSnackbar(
-            message = context.getString(R.string.onboarding_first_description),
-            actionLabel = context.getString(R.string.error_btn_retry),
-            withDismissAction = true,
-            duration = SnackbarDuration.Indefinite
-        )
-
-        when (snackbarResult) {
-            SnackbarResult.ActionPerformed -> {
-
-            }
-            SnackbarResult.Dismissed -> {
-            }
-        }
-    }
-
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        snackbarHost = {
-            SnackbarHost(
-                hostState = snackbarHostState,
-                modifier = Modifier.windowInsetsPadding(WindowInsets.navigationBarsIgnoringVisibility)
-            )
-        }
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = if (isSystemInDarkTheme) surfaceContainerDark else surfaceContainerLight)
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -69,7 +48,9 @@ fun OnboardingScreen(
                     bottom = innerPadding.calculateBottomPadding()
                 )
         ) {
+            // indicator
 
+            // pager
         }
     }
 }
