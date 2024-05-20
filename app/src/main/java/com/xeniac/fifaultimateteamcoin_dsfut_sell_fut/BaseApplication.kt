7e31807aca5402fda886c0f075e80298c0984ae3
William <william.gates.3299@gmail.com>
2024-05-20 17:13:20 +0330

    Implemented Coil ImageLoaderFactory

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/BaseApplication.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/BaseApplication.kt
@@ -6,14 +6,25 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
 import androidx.core.content.ContextCompat
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.decode.SvgDecoder
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.model.AppTheme
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.services.PickUpPlayerNotificationService
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltAndroidApp
-class BaseApplication : Application() {
+class BaseApplication : Application(), ImageLoaderFactory {
 
     @Inject
     lateinit var currentAppTheme: AppTheme
@@ -95,4 +106,45 @@ class BaseApplication : Application() {
         TapsellPlus.setGDPRConsent(this, true)
     }
      */
+
+    override fun newImageLoader(): ImageLoader = ImageLoader(context = this).newBuilder().apply {
+        components {
+            add(factory = SvgDecoder.Factory())
+            add(
+                factory = if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)) {
+                    ImageDecoderDecoder.Factory()
+                } else {
+                    GifDecoder.Factory()
+                }
+            )
+        }
+        crossfade(enable = true)
+        // Ignore the network cache headers and always read from/write to the disk cache.
+        respectCacheHeaders(enable = false)
+        memoryCachePolicy(policy = CachePolicy.ENABLED)
+        diskCachePolicy(policy = CachePolicy.ENABLED)
+        memoryCache {
+            MemoryCache.Builder(this@BaseApplication)
+                // Set the max size to 25% of the app's available memory.
+                .maxSizePercent(percent = 0.25)
+                .build()
+        }
+        diskCache {
+            DiskCache.Builder()
+                // Set cache directory folder name
+                .directory(cacheDir.resolve("image_cache"))
+                .maxSizePercent(percent = 0.03)
+                .build()
+        }
+        okHttpClient {
+            // Don't limit concurrent network requests by host.
+            val dispatcher = Dispatcher().apply { maxRequestsPerHost = maxRequests }
+
+            // Lazily create the OkHttpClient that is used for network operations.
+            OkHttpClient.Builder()
+                .dispatcher(dispatcher)
+                .build()
+        }
+        if (BuildConfig.DEBUG) logger(logger = DebugLogger())
+    }.build()
 }
