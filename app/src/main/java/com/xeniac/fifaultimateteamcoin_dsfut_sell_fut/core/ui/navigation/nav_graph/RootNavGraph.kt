e3fabbf199c6af179ffece12a7476c62f5d9c7b4
William <william.gates.3299@gmail.com>
2024-05-30 20:05:21 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/ui/navigation/nav_graph/RootNavGraph.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/ui/navigation/nav_graph/RootNavGraph.kt
@@ -1,10 +1,6 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.ui.navigation.nav_graph
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -37,15 +33,4 @@ fun SetupRootNavGraph(
             HomeScreen(rootNavController = rootNavController)
         }
     }
-}
-
-@Composable
-inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(
-    navController: NavHostController
-): T {
-    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
-    val parentEntry = remember(this) {
-        navController.getBackStackEntry(navGraphRoute)
-    }
-    return hiltViewModel(parentEntry)
 }
