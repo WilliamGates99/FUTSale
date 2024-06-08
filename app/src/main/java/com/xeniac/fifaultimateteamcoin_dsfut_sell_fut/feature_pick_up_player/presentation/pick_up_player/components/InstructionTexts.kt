319bef528a304103138c91474982de581a08e5a2
William <william.gates.3299@gmail.com>
2024-06-08 19:54:27 +0330

    Implemented Pick Up Player Screen

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/presentation/pick_up_player/components/InstructionTexts.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/presentation/pick_up_player/components/InstructionTexts.kt
@@ -1,6 +1,5 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.presentation.pick_up_player.components
 
-import android.text.SpannableStringBuilder
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,13 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.fromHtml
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.text.HtmlCompat
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.R
-import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.presentation.utils.toAnnotatedString
 
 @OptIn(ExperimentalStdlibApi::class)
 @Composable
@@ -31,15 +30,13 @@ fun InstructionTexts(
     titleLineHeight: TextUnit = 14.sp,
     titleFontWeight: FontWeight = FontWeight.Medium,
     titleColor: Color = MaterialTheme.colorScheme.onBackground,
-    instruction: String = SpannableStringBuilder(
-        stringResource(
-            id = R.string.pick_up_player_text_instruction,
-            primaryColorHex
-        )
-    ).toString(),
+    instruction: String = stringResource(
+        id = R.string.pick_up_player_text_instruction,
+        primaryColorHex
+    ),
     instructionFontSize: TextUnit = 14.sp,
-    instructionLineHeight: TextUnit = 14.sp,
-    instructionFontWeight: FontWeight = FontWeight.Medium,
+    instructionLineHeight: TextUnit = 16.sp,
+    instructionFontWeight: FontWeight = FontWeight.Normal,
     instructionColor: Color = MaterialTheme.colorScheme.onBackground
 ) {
     Column(
@@ -55,10 +52,7 @@ fun InstructionTexts(
         )
 
         Text(
-            text = HtmlCompat.fromHtml(
-                /* source = */ instruction,
-                /* flags = */ HtmlCompat.FROM_HTML_MODE_COMPACT
-            ).toAnnotatedString(),
+            text = AnnotatedString.fromHtml(instruction),
             fontSize = instructionFontSize,
             lineHeight = instructionLineHeight,
             fontWeight = instructionFontWeight,
