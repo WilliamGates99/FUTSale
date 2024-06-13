4ef19d1ac87736794a85a25600ccd015a1343c39
William <william.gates.3299@gmail.com>
2024-06-13 21:17:50 +0330

    Implemented Pick Up Player Screen

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/BaseApplication.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/BaseApplication.kt
@@ -3,6 +3,7 @@ package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut
 import android.app.Application
 import android.app.Notification
 import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.os.Build
 import androidx.core.content.ContextCompat
@@ -16,7 +17,6 @@ import coil.memory.MemoryCache
 import coil.request.CachePolicy
 import coil.util.DebugLogger
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.models.AppTheme
-import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.services.PickUpPlayerNotificationService
 import dagger.hilt.android.HiltAndroidApp
 import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
@@ -38,8 +38,9 @@ class BaseApplication : Application(), ImageLoaderFactory {
         setupTimber()
         setAppTheme()
 
-        createMutedPickUpPlayerNotificationChannel()
-        createSoundedPickUpPlayerNotificationChannel()
+        createPickUpPlayerNotificationChannelGroup()
+        createSilentPickUpPlayerNotificationChannel()
+        createDefaultPickUpPlayerNotificationChannel()
         // initAppLovin()
         // initTapsell()
     }
@@ -48,35 +49,55 @@ class BaseApplication : Application(), ImageLoaderFactory {
 
     private fun setAppTheme() = currentAppTheme.setAppTheme()
 
-    private fun createMutedPickUpPlayerNotificationChannel() {
+    private fun createPickUpPlayerNotificationChannelGroup() {
+        val notificationChannelGroup = NotificationChannelGroup(
+            /* id = */ "channel_group_pick_up_player",
+            /* name = */ getString(R.string.notification_pick_up_player_channel_group_name)
+        )
+
+        notificationManager.createNotificationChannelGroup(notificationChannelGroup)
+    }
+
+    private fun createSilentPickUpPlayerNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val pickUpPlayerNotificationChannel = NotificationChannel(
-                PickUpPlayerNotificationService.MUTED_PICK_UP_NOTIFICATION_CHANNEL_ID,
-                getString(R.string.notification_channel_name_pick_up_player),
-                NotificationManager.IMPORTANCE_HIGH
+                /* id = */ "notification_channel_pick_up_player_silent",
+                /* name = */ getString(R.string.notification_pick_up_player_channel_name_silent),
+                /* importance = */ NotificationManager.IMPORTANCE_HIGH
             ).apply {
+                group = "channel_group_pick_up_player"
                 description = getString(R.string.notification_channel_description_pick_up_player)
                 lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-                lightColor = ContextCompat.getColor(this@BaseApplication, R.color.green)
+                lightColor = ContextCompat.getColor(
+                    /* context = */ this@BaseApplication,
+                    /* id = */ R.color.green
+                )
                 enableLights(true)
                 enableVibration(false)
-                setSound(null, null)
+                setSound(
+                    /* sound = */ null,
+                    /* audioAttributes = */ null
+                )
             }
 
             notificationManager.createNotificationChannel(pickUpPlayerNotificationChannel)
         }
     }
 
-    private fun createSoundedPickUpPlayerNotificationChannel() {
+    private fun createDefaultPickUpPlayerNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val pickUpPlayerNotificationChannel = NotificationChannel(
-                PickUpPlayerNotificationService.SOUNDED_PICK_UP_NOTIFICATION_CHANNEL_ID,
-                getString(R.string.notification_channel_name_pick_up_player),
-                NotificationManager.IMPORTANCE_HIGH
+                /* id = */ "notification_channel_pick_up_player_default",
+                /* name = */ getString(R.string.notification_pick_up_player_channel_name_default),
+                /* importance = */ NotificationManager.IMPORTANCE_HIGH
             ).apply {
+                group = "channel_group_pick_up_player"
                 description = getString(R.string.notification_channel_description_pick_up_player)
                 lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-                lightColor = ContextCompat.getColor(this@BaseApplication, R.color.green)
+                lightColor = ContextCompat.getColor(
+                    /* context = */ this@BaseApplication,
+                    /* id = */ R.color.green
+                )
                 enableLights(true)
                 enableVibration(false)
             }
--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/di/AppModule.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/di/AppModule.kt
@@ -1,13 +1,9 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.di
 
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.net.ConnectivityManager
 import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
@@ -15,11 +11,9 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
-import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.R
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.data.db.FutSaleDatabase
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.models.AppTheme
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.repositories.PreferencesRepository
-import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.services.PickUpPlayerNotificationService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -44,7 +38,6 @@ import kotlinx.serialization.json.Json
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.Locale
-import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -53,19 +46,19 @@ internal object AppModule {
 
     @Provides
     @Singleton
-    fun provideConnectivityManager(
-        @ApplicationContext context: Context
-    ): ConnectivityManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        context.getSystemService(ConnectivityManager::class.java)
-    } else context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-    @Provides
-    @Singleton
     fun provideNotificationManager(
         @ApplicationContext context: Context
     ): NotificationManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
         context.getSystemService(NotificationManager::class.java)
     } else context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    @Provides
+    @Singleton
+    fun provideConnectivityManager(
+        @ApplicationContext context: Context
+    ): ConnectivityManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        context.getSystemService(ConnectivityManager::class.java)
+    } else context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     @Provides
     @Singleton
@@ -130,82 +123,9 @@ internal object AppModule {
     ): AppTheme = preferencesRepository.getCurrentAppThemeSynchronously()
 
     @Provides
-    @Named("notification_sound")
-    fun provideIsNotificationSoundActive(
-        preferencesRepository: PreferencesRepository
-    ): Boolean = preferencesRepository.isNotificationSoundActiveSynchronously()
-
-    @Provides
-    @Named("notification_vibrate")
-    fun provideIsNotificationVibrateActive(
-        preferencesRepository: PreferencesRepository
-    ): Boolean = preferencesRepository.isNotificationVibrateActiveSynchronously()
-
-    @Provides
     @Singleton
     fun provideDecimalFormat(): DecimalFormat = DecimalFormat(
         /* pattern = */ "00",
         /* symbols = */ DecimalFormatSymbols(Locale.US)
     )
-
-    @Provides
-    @Singleton
-    fun provideCancelNotificationPendingIntent(
-        @ApplicationContext context: Context
-    ): PendingIntent = PendingIntent.getActivity(
-        /* context = */ context,
-        /* requestCode = */ 0,
-        /* intent = */ Intent(""),
-        /* flags = */ if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            PendingIntent.FLAG_IMMUTABLE else 0
-    )
-
-    @Provides
-    @Singleton
-    @Named("muted_pick_up_notification")
-    fun provideMutedBasePickUpNotificationBuilder(
-        @ApplicationContext context: Context,
-        cancelNotificationPendingIntent: PendingIntent
-    ) = NotificationCompat.Builder(
-        context, PickUpPlayerNotificationService.MUTED_PICK_UP_NOTIFICATION_CHANNEL_ID
-    ).apply {
-        setAutoCancel(true)
-        setSmallIcon(R.drawable.ic_notification)
-        setContentIntent(cancelNotificationPendingIntent)
-
-        /**
-         * On Android 8.0 and above these values are ignored
-         * in favor of the values set on the notification's channel.
-         * On older platforms, these values are still used,
-         * so it is still required for apps supporting those platforms.
-         */
-        setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-        setLights(ContextCompat.getColor(context, R.color.green), 1000, 1000)
-        setSound(null)
-        setVibrate(null)
-    }
-
-    @Provides
-    @Singleton
-    @Named("sounded_pick_up_notification")
-    fun provideSoundedBasePickUpNotificationBuilder(
-        @ApplicationContext context: Context,
-        cancelNotificationPendingIntent: PendingIntent
-    ) = NotificationCompat.Builder(
-        context, PickUpPlayerNotificationService.SOUNDED_PICK_UP_NOTIFICATION_CHANNEL_ID
-    ).apply {
-        setAutoCancel(true)
-        setSmallIcon(R.drawable.ic_notification)
-        setContentIntent(cancelNotificationPendingIntent)
-
-        /**
-         * On Android 8.0 and above these values are ignored
-         * in favor of the values set on the notification's channel.
-         * On older platforms, these values are still used,
-         * so it is still required for apps supporting those platforms.
-         */
-        setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-        setLights(ContextCompat.getColor(context, R.color.green), 1000, 1000)
-        setVibrate(null)
-    }
 }
