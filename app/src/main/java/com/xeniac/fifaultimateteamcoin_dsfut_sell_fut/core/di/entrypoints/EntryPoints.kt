9477c980ff184e0395dadf02c23ea19a2f47182b
William <william.gates.3299@gmail.com>
2024-06-20 20:27:05 +0330

    Implemented Firebase Cloud Messaging

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/di/entrypoints/EntryPoints.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/di/entrypoints/EntryPoints.kt
@@ -1,9 +1,16 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.di.entrypoints
 
+import android.app.NotificationManager
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import java.text.DecimalFormat
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface NotificationManagerEntryPoint {
+    val notificationManager: NotificationManager
+}
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
