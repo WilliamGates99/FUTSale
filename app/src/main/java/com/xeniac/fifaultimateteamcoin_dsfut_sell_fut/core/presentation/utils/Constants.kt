85757d92928e2056d0c90591203e5f90f804d4d4
William <william.gates.3299@gmail.com>
2024-06-21 19:54:43 +0330

    Implemented In-App Reviews API

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/presentation/utils/Constants.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/presentation/utils/Constants.kt
@@ -5,8 +5,4 @@ object Constants {
     // Web URLs
     const val URL_DSFUT = "https://dsfut.net"
     const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/fifaultimateteamcoin/privacy_policy"
-
-    // Google Play In-App Reviews API Constants
-    const val IN_APP_REVIEWS_DAYS_FROM_FIRST_INSTALL_TIME = 10
-    const val IN_APP_REVIEWS_DAYS_FROM_PREVIOUS_REQUEST_TIME = 5
 }
