08719e1991cf81fa586ee68bf5f0bcb27d294b9d
William <william.gates.3299@gmail.com>
2024-06-28 19:04:21 +0330

    Wrote Home Tests

---
+++ app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_home/domain/use_case/CheckForAppUpdatesUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_home.domain.use_case
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.MainCoroutineRule
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_home.data.repositories.FakeHomeRepositoryImpl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(JUnit4::class)
+class CheckForAppUpdatesUseCaseTest {
+
+    @get:Rule
+    var instanceTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var fakeHomeRepository: FakeHomeRepositoryImpl
+    private lateinit var checkForAppUpdatesUseCase: CheckForAppUpdatesUseCase
+
+    @Before
+    fun setUp() {
+        fakeHomeRepository = FakeHomeRepositoryImpl()
+        checkForAppUpdatesUseCase = CheckForAppUpdatesUseCase(
+            homeRepository = fakeHomeRepository
+        )
+    }
+
+    @Test
+    fun checkForAppUpdatesWithNoUpdateAvailable_returnsNull() = runTest {
+        fakeHomeRepository.isAppUpdateAvailable(isAvailable = false)
+        val appUpdateInfo = checkForAppUpdatesUseCase().first()
+        assertThat(appUpdateInfo).isNull()
+    }
+
+    @Test
+    fun checkForAppUpdatesWithUpdateAvailable_returnsAppUpdateInfo() = runTest {
+        fakeHomeRepository.isAppUpdateAvailable(isAvailable = true)
+        val appUpdateInfo = checkForAppUpdatesUseCase().first()
+        assertThat(appUpdateInfo).isNotNull()
+    }
+}
---
+++ app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_home/domain/use_case/RequestInAppReviewsUseCaseTest.kt
@@ -0,0 +1,43 @@
+package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_home.domain.use_case
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.MainCoroutineRule
+import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_home.data.repositories.FakeHomeRepositoryImpl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(JUnit4::class)
+class RequestInAppReviewsUseCaseTest {
+
+    @get:Rule
+    var instanceTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var fakeHomeRepository: FakeHomeRepositoryImpl
+    private lateinit var requestInAppReviewsUseCase: RequestInAppReviewsUseCase
+
+    @Before
+    fun setUp() {
+        fakeHomeRepository = FakeHomeRepositoryImpl()
+        requestInAppReviewsUseCase = RequestInAppReviewsUseCase(
+            homeRepository = fakeHomeRepository
+        )
+    }
+
+    @Test
+    fun requestInAppReviewsWithNoReviewAvailable_returnsNull() = runTest {
+        fakeHomeRepository.isInAppReviewsAvailable(isAvailable = false)
+        val reviewInfo = requestInAppReviewsUseCase().first()
+        assertThat(reviewInfo).isNull()
+    }
+}
