e8d87ce0cbd68e857e3218fac9d7e347724a3078
William <william.gates.3299@gmail.com>
2024-06-28 21:23:48 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/validation/ValidatePartnerId.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/validation/ValidatePartnerId.kt
@@ -1,6 +1,5 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.domain.validation
 
-import androidx.core.text.isDigitsOnly
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.domain.utils.PickUpPlayerError
 
 class ValidatePartnerId {
@@ -9,7 +8,7 @@ class ValidatePartnerId {
             return PickUpPlayerError.BlankPartnerId
         }
 
-        if (!partnerId.isDigitsOnly()) {
+        if (partnerId.any { !it.isDigit() }) {
             return PickUpPlayerError.InvalidPartnerId
         }
 
