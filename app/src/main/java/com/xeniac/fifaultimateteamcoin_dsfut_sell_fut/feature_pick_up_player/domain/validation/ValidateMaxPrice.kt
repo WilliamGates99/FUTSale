4c103c301d07bef6e56e77553aaf0ed8b9c67489
William <william.gates.3299@gmail.com>
2024-06-29 20:43:42 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/validation/ValidateMaxPrice.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/validation/ValidateMaxPrice.kt
@@ -1,11 +1,10 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.domain.validation
 
-import androidx.core.text.isDigitsOnly
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.domain.utils.PickUpPlayerError
 
 class ValidateMaxPrice {
     operator fun invoke(maxPrice: String?): PickUpPlayerError? {
-        if (!maxPrice.isNullOrBlank() && !maxPrice.isDigitsOnly()) {
+        if (!maxPrice.isNullOrBlank() && maxPrice.any { !it.isDigit() }) {
             return PickUpPlayerError.InvalidMaxPrice
         }
 
--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/validation/ValidateMinPrice.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/validation/ValidateMinPrice.kt
@@ -1,11 +1,10 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.domain.validation
 
-import androidx.core.text.isDigitsOnly
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.domain.utils.PickUpPlayerError
 
 class ValidateMinPrice {
     operator fun invoke(minPrice: String?): PickUpPlayerError? {
-        if (!minPrice.isNullOrBlank() && !minPrice.isDigitsOnly()) {
+        if (!minPrice.isNullOrBlank() && minPrice.any { !it.isDigit() }) {
             return PickUpPlayerError.InvalidMinPrice
         }
 
