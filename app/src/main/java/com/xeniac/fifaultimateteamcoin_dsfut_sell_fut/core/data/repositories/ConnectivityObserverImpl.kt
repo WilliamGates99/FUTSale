d2b69b06402bfe9a3d4ac2341ff47847af060c43
William <william.gates.3299@gmail.com>
2024-07-01 19:38:45 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/data/repositories/ConnectivityObserverImpl.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/data/repositories/ConnectivityObserverImpl.kt
@@ -3,7 +3,6 @@ package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.data.repositories
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.Build
-import androidx.annotation.RequiresApi
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.repositories.ConnectivityObserver
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -16,35 +15,34 @@ class ConnectivityObserverImpl @Inject constructor(
     private val connectivityManager: ConnectivityManager
 ) : ConnectivityObserver {
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun observe(): Flow<ConnectivityObserver.Status> {
-        return callbackFlow {
-            val callback = object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    launch { send(ConnectivityObserver.Status.AVAILABLE) }
-                }
-
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                    super.onLosing(network, maxMsToLive)
-                    launch { send(ConnectivityObserver.Status.LOSING) }
-                }
-
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                    launch { send(ConnectivityObserver.Status.LOST) }
-                }
-
-                override fun onUnavailable() {
-                    super.onUnavailable()
-                    launch { send(ConnectivityObserver.Status.UNAVAILABLE) }
-                }
+    override fun observe(): Flow<ConnectivityObserver.Status> = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                launch { send(ConnectivityObserver.Status.AVAILABLE) }
             }
 
+            override fun onLosing(network: Network, maxMsToLive: Int) {
+                super.onLosing(network, maxMsToLive)
+                launch { send(ConnectivityObserver.Status.LOSING) }
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                launch { send(ConnectivityObserver.Status.LOST) }
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                launch { send(ConnectivityObserver.Status.UNAVAILABLE) }
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             connectivityManager.registerDefaultNetworkCallback(callback)
-            awaitClose {
-                connectivityManager.unregisterNetworkCallback(callback)
-            }
-        }.distinctUntilChanged()
-    }
+        }
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+    }.distinctUntilChanged()
 }
--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/presentation/utils/NetworkObserverHelper.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/presentation/utils/NetworkObserverHelper.kt
@@ -1,6 +1,5 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.presentation.utils
 
-import android.os.Build
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.repositories.ConnectivityObserver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,13 +13,9 @@ object NetworkObserverHelper {
 
     fun observeNetworkConnection(connectivityObserver: ConnectivityObserver) {
         CoroutineScope(Dispatchers.IO).launch {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                connectivityObserver.observe().onEach { status ->
-                    networkStatus = status
-                }.launchIn(this)
-            } else {
-                networkStatus = ConnectivityObserver.Status.AVAILABLE
-            }
+            connectivityObserver.observe().onEach { status ->
+                networkStatus = status
+            }.launchIn(this)
         }
     }
 }
