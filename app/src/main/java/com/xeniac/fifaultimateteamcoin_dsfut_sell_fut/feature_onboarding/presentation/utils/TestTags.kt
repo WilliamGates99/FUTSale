ac30bc664fa33b83d1933f73330cbba14feb861e
William <william.gates.3299@gmail.com>
2024-07-02 21:24:31 +0330

    Wrote OnboardingScreen UI Tests

---
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/presentation/utils/TestTags.kt
@@ -0,0 +1,14 @@
+package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_onboarding.presentation.utils
+
+object TestTags {
+    const val DOT_INDICATOR = "dotIndicator"
+    const val HORIZONTAL_PAGER = "horizontalPager"
+
+    const val PAGE_ONE_NEXT_BTN = "pageOneNextBtn"
+
+    const val PAGE_TWO_NEXT_BTN = "pageTwoNextBtn"
+    const val PAGE_TWO_BACK_BTN = "pageTwoBackBtn"
+
+    const val PAGE_THREE_NEXT_BTN = "pageThreeNextBtn"
+    const val PAGE_THREE_BACK_BTN = "pageThreeBackBtn"
+}
