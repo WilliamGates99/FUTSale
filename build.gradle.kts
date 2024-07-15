4efbe8bebe182d048daaba883e6b14a6c01db7cc
William <william.gates.3299@gmail.com>
2024-07-15 20:48:44 +0330

    Implemented Baseline Profiles for App Startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false // Android Gradle Plugin Version
+    alias(libs.plugins.android.test) apply false // Needed for Baseline Profiles
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
@@ -11,5 +12,4 @@ plugins {
     alias(libs.plugins.google.services) apply false // Google Services plugin
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
-    alias(libs.plugins.android.test) apply false // Needed for Baseline Profiles
 }
