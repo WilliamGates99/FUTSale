90377a06f1ab575c1dc9320f079dac411cea18f8
William <william.gates.3299@gmail.com>
2024-07-17 21:04:43 +0330

    Improved Data Store Collection Flow

--- app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/use_cases/GetIsNotificationSoundEnabledUseCaseTest.kt
+++ app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/use_cases/GetIsNotificationSoundEnabledUseCaseTest.kt
@@ -5,6 +5,7 @@ import com.google.common.truth.Truth.assertThat
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.MainCoroutineRule
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.data.repositories.FakePreferencesRepositoryImpl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -35,7 +36,7 @@ class GetIsNotificationSoundEnabledUseCaseTest {
 
     @Test
     fun getIsNotificationSoundEnabled_returnsCurrentIsNotificationSoundEnabled() = runTest {
-        val isNotificationSoundEnabled = getIsNotificationSoundEnabledUseCase()
+        val isNotificationSoundEnabled = getIsNotificationSoundEnabledUseCase().first()
         assertThat(isNotificationSoundEnabled).isEqualTo(fakePreferencesRepository.isNotificationSoundEnabled)
     }
 }
--- app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/use_cases/GetIsNotificationVibrateEnabledUseCaseTest.kt
+++ app/src/test/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/domain/use_cases/GetIsNotificationVibrateEnabledUseCaseTest.kt
@@ -5,6 +5,7 @@ import com.google.common.truth.Truth.assertThat
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.MainCoroutineRule
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.data.repositories.FakePreferencesRepositoryImpl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -35,7 +36,7 @@ class GetIsNotificationVibrateEnabledUseCaseTest {
 
     @Test
     fun getIsNotificationVibrateEnabled_returnsCurrentIsNotificationVibrateEnabled() = runTest {
-        val isNotificationVibrateEnabled = getIsNotificationVibrateEnabledUseCase()
+        val isNotificationVibrateEnabled = getIsNotificationVibrateEnabledUseCase().first()
         assertThat(isNotificationVibrateEnabled).isEqualTo(fakePreferencesRepository.isNotificationVibrateEnabled)
     }
 }
