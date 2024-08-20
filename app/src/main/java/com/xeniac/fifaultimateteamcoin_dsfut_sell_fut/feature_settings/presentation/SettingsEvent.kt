79926e8f9935f4aa215f511b3a5d2f9fa64e7d8b
William <william.gates.3299@gmail.com>
2024-08-20 17:33:35 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_settings/presentation/SettingsEvent.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_settings/presentation/SettingsEvent.kt
@@ -3,9 +3,9 @@ package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_settings.presenta
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.models.AppLocale
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.models.AppTheme
 
-sealed class SettingsEvent {
-    data class SetCurrentAppLocale(val newAppLocale: AppLocale) : SettingsEvent()
-    data class SetCurrentAppTheme(val newAppTheme: AppTheme) : SettingsEvent()
-    data class SetNotificationSoundSwitch(val isEnabled: Boolean) : SettingsEvent()
-    data class SetNotificationVibrateSwitch(val isEnabled: Boolean) : SettingsEvent()
+sealed interface SettingsEvent {
+    data class SetCurrentAppLocale(val newAppLocale: AppLocale) : SettingsEvent
+    data class SetCurrentAppTheme(val newAppTheme: AppTheme) : SettingsEvent
+    data class SetNotificationSoundSwitch(val isEnabled: Boolean) : SettingsEvent
+    data class SetNotificationVibrateSwitch(val isEnabled: Boolean) : SettingsEvent
 }
