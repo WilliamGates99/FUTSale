1f99df71e00349eeacbed01ed3e93e6b69b91a20
William <william.gates.3299@gmail.com>
2024-08-20 16:18:46 +0330

    Improved MD5 Hash Generator

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/data/utils/HashHelper.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_pick_up_player/data/utils/HashHelper.kt
@@ -1,25 +1,20 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_pick_up_player.data.utils
 
-import java.math.BigInteger
 import java.security.MessageDigest
 
 object HashHelper {
 
+    @OptIn(ExperimentalStdlibApi::class)
     fun getMd5Signature(
         partnerId: String,
         secretKey: String,
         timestamp: Long
     ): String {
         val input = partnerId + secretKey + timestamp
-        val md = MessageDigest.getInstance(/* algorithm = */ "MD5")
 
-        return BigInteger(
-            /* signum = */ 1,
-            /* magnitude = */ md.digest(input.toByteArray())
-        ).toString(/* radix = */ 16)
-            .padStart(
-                length = 23,
-                padChar = '0'
-            )
+        val md = MessageDigest.getInstance(/* algorithm = */ "MD5")
+        val digest = md.digest(input.toByteArray())
+
+        return digest.toHexString()
     }
 }
