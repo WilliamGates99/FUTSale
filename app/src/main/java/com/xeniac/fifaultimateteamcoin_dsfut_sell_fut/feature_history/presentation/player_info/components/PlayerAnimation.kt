46a9b7b616486ba522ce8a2cc41183931d0e7368
William <william.gates.3299@gmail.com>
2024-08-27 18:33:30 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_history/presentation/player_info/components/PlayerAnimation.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_history/presentation/player_info/components/PlayerAnimation.kt
@@ -1,11 +1,13 @@
 package com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_history.presentation.player_info.components
 
-import androidx.annotation.RawRes
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -15,14 +17,25 @@ import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.R
 @Composable
 fun PlayerAnimation(
     modifier: Modifier = Modifier,
-    @RawRes lottieAnimation: Int = R.raw.anim_history_player
+    layoutDirection: LayoutDirection = LocalLayoutDirection.current,
+    animationComposition: LottieComposition? = rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.anim_history_player)
+    ).value,
+    animationIteration: Int = LottieConstants.IterateForever,
+    animationSpeed: Float = 0.75f,
+    animationRotationDegree: Float = when (layoutDirection) {
+        LayoutDirection.Ltr -> 0f
+        LayoutDirection.Rtl -> 180f
+    },
 ) {
-    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(lottieAnimation))
-
     LottieAnimation(
-        composition = composition,
-        speed = 0.75f,
-        iterations = LottieConstants.IterateForever,
-        modifier = modifier.size(250.dp)
+        composition = animationComposition,
+        iterations = animationIteration,
+        speed = animationSpeed,
+        modifier = modifier
+            .size(250.dp)
+            .graphicsLayer {
+                rotationY = animationRotationDegree
+            }
     )
 }
