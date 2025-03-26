623cc2f8a91b2349a7cd51438b8f42a3ba27caad
William <william.gates.3299@gmail.com>
2025-03-26 19:28:55 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/domain/use_case/CompleteOnboardingUseCase.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/feature_onboarding/domain/use_case/CompleteOnboardingUseCase.kt
@@ -4,9 +4,8 @@ import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.repositories.D
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.repositories.SettingsDataStoreRepository
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.domain.utils.Result
 import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.feature_onboarding.domain.utils.OnboardingError
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
 
 class CompleteOnboardingUseCase(
     private val settingsDataStoreRepository: SettingsDataStoreRepository,
@@ -15,17 +14,15 @@ class CompleteOnboardingUseCase(
     operator fun invoke(
         partnerId: String?,
         secretKey: String?
-    ): Flow<Result<Unit, OnboardingError>> = callbackFlow {
-        try {
+    ): Flow<Result<Unit, OnboardingError>> = flow {
+        return@flow try {
             dsfutDataStoreRepository.storePartnerId(partnerId = partnerId)
             dsfutDataStoreRepository.storeSecretKey(secretKey = secretKey)
             settingsDataStoreRepository.isOnboardingCompleted(isCompleted = true)
-            send(Result.Success(Unit))
+            emit(Result.Success(Unit))
         } catch (e: Exception) {
             e.printStackTrace()
-            send(Result.Error(OnboardingError.SomethingWentWrong))
+            emit(Result.Error(OnboardingError.SomethingWentWrong))
         }
-
-        awaitClose { }
     }
 }
