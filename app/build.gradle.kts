45655fcfaf622f2daae80032f4aa4790f33f922d
William <william.gates.3299@gmail.com>
2025-03-29 14:14:03 +0330

    v2.1.3 (30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.xeniac.fifaultimateteamcoin_dsfut_sell_fut"
         minSdk = 21
         targetSdk = 36
-        versionCode = 29
-        versionName = "2.1.2"
+        versionCode = 30
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.HiltTestRunner"
 
